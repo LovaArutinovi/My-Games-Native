b16e9421b66668ffafe1f68aa16af6e135238597
LovaArutinovi <arutinov58@gmail.com>
2022-06-15 00:32:23 +0400

    start

---
+++ screens/ModalScreen.tsx
@@ -0,0 +1,44 @@
+import { StatusBar } from "expo-status-bar";
+import { Platform, StyleSheet } from "react-native";
+
+import EditScreenInfo from "../components/EditScreenInfo";
+import { Text, View } from "../components/Themed";
+
+export default function ModalScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Modal</Text>
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
+      {/* <EditScreenInfo path="/screens/ModalScreen.tsx" /> */}
+      <Text style={styles.title}>
+        hanaqem anum ch jxainanas to xoz u xaim txa
+      </Text>
+
+      {/* Use a light status bar on iOS to account for the black space above the modal */}
+      <StatusBar style={Platform.OS === "ios" ? "light" : "auto"} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    paddingHorizontal: 20,
+    fontSize: 20,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: "80%",
+  },
+});
